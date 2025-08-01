45c934343b6dfbf0bfa7ec47c11bfb5c4a35958b
Rahul-kewat-1 <rahul.kewat@negroup.com>
2025-08-01 16:18:59 +0530

    usecase commit

--- src/main/java/com/promotion/PromotionService.java
+++ src/main/java/com/promotion/PromotionService.java
@@ -3,7 +3,7 @@ package com.promotion;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.promotion")
 public class PromotionService {
     public static void main(String[] args) {
         SpringApplication.run(PromotionService.class, args);
