e8f475a9afaa1709eaa059bdb95d6fe85056bfb7
Rahul-kewat-1 <rahul.kewat@negroup.com>
2025-08-01 16:03:07 +0530

    usecase commit

--- src/main/java/com/example/demo/PromotionService.java
+++ src/main/java/com/example/demo/PromotionService.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class PromotionService {
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(PromotionService.class, args);
     }
 }
--- src/main/java/com/example/demo/model/HelloResponse.java
+++
@@ -1,3 +0,0 @@
-package com.example.demo.model;
-
-public record HelloResponse(String message) {}
