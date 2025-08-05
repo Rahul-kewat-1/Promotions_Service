87fc3c0ae8fd964a51d035c102d6391051b5e30f
Rahul-kewat-1 <rahul.kewat@negroup.com>
2025-08-05 13:20:49 +0530

    minor change

---
+++ src/main/java/com/promotion/config/TraceFilter.java
@@ -0,0 +1,24 @@
+package com.promotion.config;
+
+import jakarta.servlet.*;
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Component
+public class TraceFilter implements Filter {
+    public static final String TRACE_ID = "traceId";
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        String traceId = UUID.randomUUID().toString();
+        MDC.put(TRACE_ID, traceId);
+        try {
+            chain.doFilter(request, response);
+        } finally {
+            MDC.remove(TRACE_ID); // Clean up after request
+        }
+    }
+}
